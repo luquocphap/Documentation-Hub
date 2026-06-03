bc558c1d57e8f05a32993cfa68b4939360326568
luquocphap <phap.luquoc@hcmut.edu.vn>
2026-06-03 11:25:30 +0700

    🔧 fix: validate input for register/login

--- src/modules-api/auth/dto/register.dto.ts
+++ src/modules-api/auth/dto/register.dto.ts
@@ -1,17 +1,25 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsOptional } from "class-validator";
+import { IsNotEmpty } from "class-validator";
+import { IsEmailWhenNotEmpty } from "src/common/decorators/is-email-when-not-empty.decorator";
+import { IsLengthWhenNotEmpty } from "src/common/decorators/is-length-when-not-empty.decorator";
 
 export class RegisterBody {
-    @IsNotEmpty()
-    @IsEmail(undefined, { message: "Invalid Email" })
+    @IsNotEmpty({message: "Mandatory field"})
+    @ApiProperty({
+        description: "User email address",
+         example: "luphap@gmail.com",
+         format: "email",
+    })
+    @IsEmailWhenNotEmpty({ message: "Invalid Email" })
     @ApiProperty({ example: "luphap@gmail.com" })
     email!: string;
 
-    @IsNotEmpty()
-    @ApiProperty({ example: "123456" })
+    @IsNotEmpty({message: "Mandatory field"})
+    @IsLengthWhenNotEmpty(8)
+    @ApiProperty({ example: "12345678" })
     password!: string;
 
-    @IsNotEmpty()
+    @IsNotEmpty({message: "Mandatory field"})
     @ApiProperty({ example: "Lu Quoc Phap"})
     fullName!: string;
 }
